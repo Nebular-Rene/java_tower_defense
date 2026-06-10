ea79b43c5a3cd7d796c39d4d45ed3d13428b28d7
Alex5X5 <alex.25.lehnert@gmail.com>
2026-06-10 11:32:26 +0200

    updated compile.bat

--- src/main/java/compile.bat
+++ src/main/java/compile.bat
@@ -1,2 +1,2 @@
-C:\Users\lehnerale\Java\sdk\jdk-26.0.1\bin\javac.exe *.java
-C:\Users\lehnerale\Java\sdk\jdk-26.0.1\bin\java.exe Main
+%UserProfile%\Java\sdk\jdk-26.0.1\bin\javac.exe *.java
+%UserProfile%\Java\sdk\jdk-26.0.1\bin\java.exe Main
